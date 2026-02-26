e644f31b8bb455361737202b70ac23c4227d9fbf
oxford13zero <oxford13@gmail.com>
2026-02-25 16:26:03 -0800

    Update routing-config.js

--- api/routing-config.js
+++ api/routing-config.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   const { data: school, error: schoolError } = await supabaseAdmin
     .from("schools")
     .select("country")
-    .eq("id", auth.school_id)
+    .eq("id", auth.school.id)
     .maybeSingle();
 
   if (schoolError || !school) {
